4c3a1ec49236f74a51a2d40d9458b1cbd03b7300
Suraj Muraleedharan <surajcm@gmail.com>
2024-03-19 09:55:44 +0530

    initial commit

---
+++ src/main/kotlin/com/github/surajcm/openRestaurant/OpenRestaurantApplication.kt
@@ -0,0 +1,11 @@
+package com.github.surajcm.openRestaurant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenRestaurantApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenRestaurantApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/surajcm/openRestaurent/OpenRestaurentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.surajcm.openRestaurent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenRestaurentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
